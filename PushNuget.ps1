0d996badc9e23ae575d89a1f2def70d239932855
nicolas.dorier <nicolas.dorier@gmail.com>
2018-08-24 18:03:00 +0900

    Fix push nuget

--- PushNuget.ps1
+++ PushNuget.ps1
@@ -1,6 +1,6 @@
-Remove-Item "bin\release\" -Recurse -Force
+Remove-Item "src\bin\release\" -Recurse -Force
 dotnet pack --configuration Release
-dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
-$ver = ((Get-ChildItem .\bin\release\*.nupkg)[0].Name -replace '[^\d]*\.(\d+(\.\d+){1,4}).*', '$1')
+dotnet nuget push "src\bin\Release\" --source "https://api.nuget.org/v3/index.json"
+$ver = ((Get-ChildItem .src\bin\release\*.nupkg)[0].Name -replace '[^\d]*\.(\d+(\.\d+){1,4}).*', '$1')
 git tag -a "v$ver" -m "$ver"
 git push --tags
