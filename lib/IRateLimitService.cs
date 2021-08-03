c4635644f081d6a96f9b5776aa81ea8c2b594a45
Sabine Pölzlbauer <sabine.poelzlbauer@go-springtime.com>
2021-08-03 16:08:39 +0200

    extracted an IRateLimitService Interface for the RateLimitService (to allow injecting a custom RateLimitService for Tests)

---
+++ lib/IRateLimitService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace NicolasDorier.RateLimits
+{
+    public interface IRateLimitService
+    {
+        int BucketsCount { get; }
+        void SetZone(LimitRequestZone requestZone);
+        void SetZone(string requestZone);
+        Task<bool> Throttle(string zoneName, object scope = null, CancellationToken cancellationToken = default);
+    }
+}
